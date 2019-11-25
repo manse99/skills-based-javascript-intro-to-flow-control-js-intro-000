1ac3d7799cbc96dc11cb13514a357620ca9dff17
GC <manse99@gmail.com>
2019-11-25 21:37:43 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,12 @@
 function basicTeenager(age) {
-
+if age == 13 >= 19;
+return "You are a teenager!";
 }
 
 function teenager(age) {
-
+  if (age == 13 && < 19)
+   "you are a teenager"
+else  "you are not a teenager"
 }
 
 function ageChecker(age) {
